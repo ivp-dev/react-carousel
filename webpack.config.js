3a4e04043eab101a3afc85d93d126202d981b430
ivp.net <ivp.net@outlook.com>
2021-02-22 22:04:06 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [ 'babel-loader', 'ts-loader' ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
